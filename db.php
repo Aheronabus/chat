eb994ce589d6873b2209eece6c316282811e77a8
Gaos <ccubillos650@gmail.com>
2018-12-22 18:14:10 -0500

    first Commit. Pre-Alpha 1.0. Se añade posibilidad de chatear y menu

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+$servidor = "localhost";
+$usuario= "root";
+$password = "Marioloco456";
+$base_datos = "appchat";
+
+
+
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
+
+
+function formatearFecha($fecha){
+	return date('g:i a', strtotime($fecha));
+}
+
+
+?>
